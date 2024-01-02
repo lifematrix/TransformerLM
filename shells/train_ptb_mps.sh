429159cfa013d92d2e6777788febc83a36a1e98e
Liu Congxin <liucongxin@airdoc.com>
2024-01-02 12:38:46 +0800

    add folder placeholder

---
+++ shells/train_ptb_mps.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+export PYTHONPATH=src/python:$PYTHONPATH
+
+python -m transformerlm.train_ptb --device cuda --num_iters 100000  \
+                    --steps_per_eval 20000 \
+                    --batch_size 2  \
+                    --eval_test     \
+                    --save_checkpoint checkpoint/lm_20240101
