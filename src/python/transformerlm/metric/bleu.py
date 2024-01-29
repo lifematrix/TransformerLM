69c47a60a1d30c8c6fae0d999bd54981609691c4
Steven Liu <stevenliucx@gmail.com>
2024-01-29 23:33:18 +0800

    fix bleu bug

--- src/python/transformerlm/metric/bleu.py
+++ src/python/transformerlm/metric/bleu.py
@@ -6,6 +6,7 @@ Compute the BLEU score to evaluation of Neural Machine Learning Model.
 import math
 from collections import Counter
 import sacrebleu
+from sacrebleu.tokenizers.tokenizer_13a import Tokenizer13a
 
 class BLEUScore:
     def __init__(self):
@@ -19,9 +20,13 @@ class BLEUScore:
         self.precisions = None
         self.weights = None
         self.sentence_score = None
+
     def __str__(self):
-        return (f"BLEU Score: {self.sentence_score} ( Precision: {self.precisions} | Counts: {self.counts} Matches: {self.matches} |"
-                f" BP: {self.BP} Effective Reference Length: {self.refs_len} Hypothesis Lenght: {self.hyp_len} | max_ngram: f{self.max_ngram}")
+        return (f"BLEU Score: {self.sentence_score} (Precisions: {' / '.join(['%.4f'%x for x in self.precisions])} | "
+                f"Counts: {self.counts}, Matches: {self.matches} |"
+                f" BP: {self.BP}, Effective Reference Length: {self.effective_ref_len}, Hypothesis Length: {self.hyp_len},"
+                f" References Length: {self.refs_len} | Max ngrams: {self.max_ngram}, "
+                f" Weights: {self.weights})")
 
 
 class BLEU:
@@ -39,7 +44,7 @@ class BLEU:
         self.smooth_method = smooth_method
         self.invcnt = 1
 
-        self.tokenizer = sacrebleu.tokenizers.tokenizer_13a.Tokenizer13a()
+        self.tokenizer = Tokenizer13a()
 
     def tokenize(self, text):
         text = self.tokenizer(text)
@@ -49,8 +54,7 @@ class BLEU:
 
 
     def count_ngrams(self, text, max_ngrams):
-        text = self.tokenize(text)
-        tokens = text.lower().split()
+        tokens = self.tokenize(text.lower())
         cnr = Counter()
         for ngrams in range(1, max_ngrams+1):
             ngram_segments = [tuple(tokens[i:i+ngrams]) for i in range(len(tokens)-ngrams+1)]
@@ -66,7 +70,7 @@ class BLEU:
             cnr, l = self.count_ngrams(ref_text, max_ngrams)
             refs_len.append(l)
             if refs_cnr is None:
-                ref_cnr = cnr
+                refs_cnr = cnr
             else:
                 for k, c in cnr.items():
                     refs_cnr[k] = max(c, refs_cnr.get(k, 0))
@@ -81,7 +85,7 @@ class BLEU:
         r = sorted_refs_len[0]
 
         c = hyp_len
-        bp = 1 if c >= r else math.exp(1 - r / c)
+        bp = 1.0 if c >= r else math.exp(1 - r / c)
 
         return bp, r
 
@@ -102,8 +106,8 @@ class BLEU:
         for k, c in hyp_cnr.items():
             n = len(k)
             n_matches = min(c, refs_cnr.get(k, 0))
-            bs.counts[n] = n
-            bs.matches[n] = n_matches
+            bs.counts[n-1] += c
+            bs.matches[n-1] += n_matches
 
         return bs
 
@@ -116,16 +120,18 @@ class BLEU:
 
         bs.precisions = [0.0] * max_ngrams
         invcnt = self.invcnt
-        for i in range(1, max_ngrams+1):
-            if bs.matches[i-1] > 0:
-                n_matches = bs.matches[i-1]
+        for i in range(max_ngrams):
+            if bs.counts[i] == 0:
+                continue
+            if bs.matches[i] > 0:
+                n_matches = bs.matches[i]
             else:
                 invcnt *= 2
                 n_matches = 1/invcnt
             bs.precisions[i] = n_matches/bs.counts[i]
 
         bs.weights = weights
-        wsum_log_ps = sum([math.log(p) * w for p, w in zip(bs.precisions, bs.weights)])
+        wsum_log_ps = sum([math.log(p) * w for p, w in zip(bs.precisions, bs.weights) if p > 0.0])
         bs.sentence_score = bs.BP * math.exp(wsum_log_ps) * 100
 
         return bs
@@ -168,8 +174,10 @@ def test_sentence_score():
     }
 
     blue = BLEU()
-    print(blue.sentence_score(example_1['Candidate'], example_1['Reference'], max_ngrams=4))
-    print(blue.sentence_score(example_2['Candidate'], example_2['Reference'], max_ngrams=4))
+    # print(blue.sentence_score(example_1['Candidate'][0], example_1['Reference'], max_ngrams=4))
+    # print(blue.sentence_score(example_1['Candidate'][1], example_1['Reference'], max_ngrams=4))
+    # print(blue.sentence_score(example_2['Candidate'][0], example_2['Reference'], max_ngrams=4))
+    print(blue.sentence_score(example_3['Candidate'][0], example_3['Reference'], max_ngrams=4))
 
 
 if __name__ == "__main__":
